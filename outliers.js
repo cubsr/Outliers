44af0889e0473df4766b74213023c5326d582f2b
cubsr <97184952+cubsr@users.noreply.github.com>
2022-08-19 11:40:45 -0600

    Fixes and updates
    Fixed multiple calculation issues and added more information to the object being returned along with cleaning up the code

--- outliers.js
+++ outliers.js
@@ -1,169 +1,157 @@
-const fs = require('fs');
-var data = JSON.parse(fs.readFileSync("MyData.json"));
-
-get_outliers(data);
-
-function get_outliers(mydata){//returns an object containing arrays of the sorted data and top and bottom difference outliers
-  let high_scores_upper = [0, 0, 0, 0, 0, 0]; //stores the larger number of the larger difference
-  let high_scores_lower = [0, 0, 0, 0, 0, 0]; //stores the smaller number of the larger difference
-  let high_scores_names = ["", "", "", "", "", ""]; //stores the combined formatted names of the high_scores
-  let high_scores_demographic_type = ["", "", "", "", "", ""]; //stores the type or key3
-  let high_scores_demographic_upper = ["", "", "", "", "", ""]; //stores theactual value of key or key2/keycheck2 for the higher value
-  let high_scores_demographic_lower = ["", "", "", "", "", ""]; //stores theactual value of key or key2/keycheck2 for the lower value
-  let high_scores_subcategory = ["", "", "", "", "", ""]; //stores key/keycheck
-  let low_scores_demographic_type = ["", "", "", "", "", ""]; //stores the type or key3
-  let low_scores_demographic_upper = ["", "", "", "", "", ""]; //stores theactual value of key or key2/keycheck2 for the higher value
-  let low_scores_demographic_lower = ["", "", "", "", "", ""]; //stores theactual value of key or key2/keycheck2 for the lower value
-  let low_scores_subcategory = ["", "", "", "", "", ""]; //stores key/keycheck
-  let low_scores_lower = [1000, 1000, 1000, 1000, 1000, 1000]; //stores the smaller number of the smaller difference
-  let low_scores_upper = [1000, 1000, 1000, 1000, 1000, 1000]; //stores the larger number of the smaller difference
-  let low_scores_names = ["", "", "", "", "", ""];//stores the combined formatted names of the smaller scores
-  let difference_high = [0, 0, 0, 0, 0, 0]; //the 6 largest differences in the data
-  let difference_low = [1000, 1000, 1000, 1000, 1000, 1000]; //the 6 smallest differences in the data
-  for (let key4 in mydata) { //these for loops unwrap the JSON
-    for (let key3 in mydata[key4]) {
-      for (let key2 in mydata[key4][key3]) {
-        for (let key in mydata[key4][key3][key2]) {//key is the lowest level so mydata[key4][key3][key2][key] retrieves the data
-          for (let keycheck2 in mydata[key4][key3]) { //keycheck2 allows for checking the subcatagories of key3 so k3 is color and key2 and keycheck2 are red and green
-            for (let keycheck in mydata[key4][key3][keycheck2]) {//keycheck is the lower level of all the checks ie color: green: shade <-
-              if(keycheck !== "count" && keycheck === key && keycheck2 !== key2 && mydata[key4][key3][keycheck2][keycheck] !== null && mydata[key4][key3][key2][key]){ //makes sure data is checking correct categories
-                let difference = 0;
-                if(mydata[key4][key3][keycheck2][keycheck] > mydata[key4][key3][key2][key]){
-                  difference = mydata[key4][key3][keycheck2][keycheck] - mydata[key4][key3][key2][key];
-                }else{
-                  difference = mydata[key4][key3][key2][key] - mydata[key4][key3][keycheck2][keycheck];
-                }
-                if(!array_value_check(high_scores_names, key3 + ": " + keycheck2 + ": " + keycheck + " > " + key2 + ": " + key) && !array_value_check(high_scores_names, key3 + ": " + key2 + ": " + key + " > " + keycheck2 + ": " + keycheck)){ //checks if we already found this data and stored it
-                  if(array_difference_check(difference_high, difference, true)){ //checks if this is a new high score
-                    let position = array_difference_locator(difference_high, difference, true); //finds the spot to put the new data in
-                    if(position >= 0){//this if section updates all the arrays to the new sorted scores
-                      difference_high = array_value_mover(difference_high, difference, position);
-                      if(mydata[key4][key3][keycheck2][keycheck] > mydata[key4][key3][key2][key]){//formatting which number is bigger
-                        high_scores_names = array_value_mover(high_scores_names, key3 + ": " + keycheck2 + ": " + keycheck + " > " + key2 + ": " + key, position);
-                        high_scores_upper = array_value_mover(high_scores_upper, mydata[key4][key3][keycheck2][keycheck], position);
-                        high_scores_lower = array_value_mover(high_scores_lower, mydata[key4][key3][key2][key], position);
-                        high_scores_demographic_type = array_value_mover(high_scores_demographic_type, key3, position);
-                        high_scores_demographic_upper = array_value_mover(high_scores_demographic_upper, keycheck2, position);
-                        high_scores_demographic_lower = array_value_mover(high_scores_demographic_lower, key2, position);
-                        high_scores_subcategory = array_value_mover(high_scores_subcategory, keycheck, position);
-                      }else{
-                        high_scores_names = array_value_mover(high_scores_names, key3 + ": " + key2 + ": " + key + " > " + keycheck2 + ": " + keycheck, position);
-                        high_scores_upper = array_value_mover(high_scores_upper, mydata[key4][key3][key2][key], position);
-                        high_scores_lower = array_value_mover(high_scores_lower, mydata[key4][key3][keycheck2][keycheck], position);
-                        high_scores_demographic_type = array_value_mover(high_scores_demographic_type, key3, position);
-                        high_scores_demographic_upper = array_value_mover(high_scores_demographic_upper, key2, position);
-                        high_scores_demographic_lower = array_value_mover(high_scores_demographic_lower, keycheck2, position);
-                        high_scores_subcategory = array_value_mover(high_scores_subcategory, keycheck, position);
-                      }
-                    }
-                  }
-                }
-                if(!array_value_check(low_scores_names, key3 + ": " + keycheck2 + ": " + keycheck + " > " + key2 + ": " + key) && !array_value_check(low_scores_names, key3 + ": " + key2 + ": " + key + " > " + keycheck2 + ": " + keycheck)){ //same operations as the high scores but for low differences
-                   if(array_difference_check(difference_low, difference, false)){//checks if this is a new low score
-                     let position = array_difference_locator(difference_low, difference, false);
-                     if(position >= 0){//this if section updates all the arrays to the new sorted scores
-                       difference_low = array_value_mover(difference_low, difference, position);
-                       if(mydata[key4][key3][keycheck2][keycheck] > mydata[key4][key3][key2][key]){//formatting which number is bigger
-                         low_scores_names = array_value_mover(low_scores_names, key3 + ": " + keycheck2 + ": " + keycheck + " > " + key2 + ": " + key, position);
-                         low_scores_upper = array_value_mover(low_scores_upper, mydata[key4][key3][keycheck2][keycheck], position);
-                         low_scores_lower = array_value_mover(low_scores_lower, mydata[key4][key3][key2][key], position);
-                         low_scores_demographic_type = array_value_mover(low_scores_demographic_type, key3, position);
-                         low_scores_demographic_upper = array_value_mover(low_scores_demographic_upper, keycheck2, position);
-                         low_scores_demographic_lower = array_value_mover(low_scores_demographic_lower, key2, position);
-                         low_scores_subcategory = array_value_mover(low_scores_subcategory, keycheck, position);
-                       }else{
-                         low_scores_names = array_value_mover(low_scores_names, key3 + ": " + key2 + ": " + key + " > " + keycheck2 + ": " + keycheck, position);
-                         low_scores_upper = array_value_mover(low_scores_upper, mydata[key4][key3][key2][key], position);
-                         low_scores_lower = array_value_mover(low_scores_lower, mydata[key4][key3][keycheck2][keycheck], position);
-                         low_scores_demographic_type = array_value_mover(low_scores_demographic_type, key3, position);
-                         low_scores_demographic_upper = array_value_mover(low_scores_demographic_upper, key2, position);
-                         low_scores_demographic_lower = array_value_mover(low_scores_demographic_lower, keycheck2, position);
-                         low_scores_subcategory = array_value_mover(low_scores_subcategory, keycheck, position);
-                       }
-                     }
-                   }
+function getTVPOutliers(mydata) {
+  let highVariance = [[0, {}], [0, {}], [0, {}], [0, {}], [0, {}]];
+  let lowVariance = [[1000, {}], [1000, {}], [1000, {}], [1000, {}], [1000, {}]];
+  for(let groupType in mydata){ //groupType shuffles through and stores the overall catagory
+    for(let group in mydata[groupType]){ //group shuffles through and stores the specific group from within grouptType
+      for(let subCategory in mydata[groupType][group]){ //subCatagory shuffles through and stores the specfic results each group has
+        value = parseFloat(mydata[groupType][group][subCategory]);
+        for(let comparisonGroup in mydata[groupType]){//this compares the current group with the other gruops subcategory results
+          if(group !== comparisonGroup && !array_value_check(highVariance, subCategory, group, comparisonGroup) && !array_value_check(lowVariance, subCategory, group, comparisonGroup)){
+            value2 = parseFloat(mydata[groupType][comparisonGroup][subCategory]);
+            difference = Math.abs(value - value2).toFixed(4);
+            if(array_difference_check(highVariance, difference, true)){
+              position = array_difference_locator(highVariance, difference, true);
+              let updatedDataStorage = {"groupType": groupType, "subCategory": subCategory, [group]: value, [comparisonGroup]: value2, allOtherGroups: {}};
+              for(let findAllGroups in mydata[groupType]){
+                if(findAllGroups !== group && findAllGroups !== comparisonGroup){
+                  updatedDataStorage.allOtherGroups[findAllGroups] = mydata[groupType][findAllGroups][subCategory];
                 }
               }
+              highVariance = array_value_insertion_handler(highVariance, subCategory, groupType, position, difference, updatedDataStorage, true);
+            }
+            if(array_difference_check(lowVariance, difference, false)){
+              position = array_difference_locator(lowVariance, difference, false);
+              let updatedDataStorage = {"groupType": groupType, "subCategory": subCategory, [group]: value, [comparisonGroup]: value2, allOtherGroups: {}};
+                for(let findAllGroups in mydata[groupType]){
+                  if(findAllGroups !== group && findAllGroups !== comparisonGroup){
+                    updatedDataStorage.allOtherGroups[findAllGroups] = mydata[groupType][findAllGroups][subCategory];
+                  }
+                }
+              lowVariance = array_value_insertion_handler(lowVariance, subCategory, groupType, position, difference, updatedDataStorage, false);
             }
           }
         }
       }
     }
   }
-  //elements of the object should be accessible through obj[difference_high[1]]; etc
-  let obj = {high_outliers: {labels:{high_scores_demographic_type, high_scores_subcategory}, data:{upper:{high_scores_demographic_upper, high_scores_upper}, lower:{high_scores_demographic_lower, high_scores_lower}, difference_high}}, lowOutliers:{labels: {low_scores_demographic_type, low_scores_subcategory}, data: {upper:{low_scores_demographic_upper, low_scores_upper}, lower:{low_scores_demographic_lower, low_scores_lower}, difference_low}}};
-
-  for(let i = 0; i < high_scores_upper.length; i++){
-    console.log(difference_high[i] + " " + high_scores_upper[i] + " " + high_scores_names[i] + " " + high_scores_lower[i]);
-  }
-  for(let i = 0; i < low_scores_upper.length; i++){
-    console.log(difference_low[i] + " " + low_scores_demographic_type[1] + ": " + low_scores_demographic_upper[i] + " " + low_scores_subcategory[i] + ": " + low_scores_upper[i] + " > " + low_scores_demographic_lower[i] + " " + low_scores_subcategory[i] + ": " + low_scores_lower[i]);
-  }//These for loops are for checking data
-
+  let obj = {highVariance, lowVariance};
   return obj;
 }
 
-function array_difference_check(array, value, high){ //this function checks if the value is higher or lower than other values in the array the high variable is true for greater than and false for less than
-  if(high){
-    for(let key in array){
-      if (array[key] < value){
+function array_difference_check(array, value, high) {
+  //this function checks if the value is higher or lower than other values in the array the high variable is true for greater than and false for less than
+  if (high) {
+    for (let i = 0; i < array.length; i++) {
+      if (parseFloat(array[i][0]) < value) {
         return true;
       }
     }
-  }else{
-    for(let key in array){
-      if (array[key] > value){
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      if (parseFloat(array[i][0]) > value) {
         return true;
       }
     }
   }
-  return false
+  return false;
 }
 
-function array_value_mover(array, value, position){ //This shifts the elements of an array from the given position down dropping the last element
-  let temp = array[position];
-  array[position] = value;
+/*function array_value_mover(array, value, obj, position) {
+  //This shifts the elements of an array from the given position down dropping the last element
+  let tempNumber = array[position][0];
+  let tempObj = array[position][1];
+  array[position][0] = value;
+  array[position][1] = obj;
   position++;
-  for(position; position < array.lenth; position++){
-    let temp2 = array[position];
-    array[position] = temp;
-    temp = temp2;
+  if(position < array.length){
+    for (position; position < array.length; position++) {
+      let tempNumber2 = array[position][0];
+      let tempObj2 = array[position][1];
+      array[position][0] = tempNumber;
+      array[position][1] = tempObj;
+      tempNumber = tempNumber2;
+      tempObj = tempObj2
+    }
   }
   return array;
-}
+}*/
 
-function array_difference_locator(array, value, high){//This finds where the new different value belongs in the array
-  let temp = 0;
-  for(let key in array){
-    if(high){
-      if (array[key] < value){
-        return parseInt(key);
+function array_difference_locator(array, value, high) {
+  //This finds where the new different value belongs in the array
+  for (let i = 0; i < array.length; i++) {
+    if (high) {
+      if (parseFloat(array[i][0]) < value) {
+        return parseInt(i);
       }
-    }else{
-      if (array[key] > value){
-        return parseInt(key);
+    } else {
+      if (parseFloat(array[i][0]) > value) {
+        return parseInt(i);
       }
     }
   }
   return -1;
 }
 
-function array_value_check(array, value){//This checks if a given value is in an array
-  for(let key in array){
-    if (array[key] === value){
-      return true;
+function array_value_check(array, subCategory, group1, group2) {
+  //This finds if 2 values are in the object
+  for (let i = 0; i < array.length; i++) {
+    if("subCategory" in array[i][1]){
+      if (subCategory.toString() === array[i][1].subCategory.toString()) {
+        if(group1 in array[i][1] && group2 in array[i][1]){
+          return true;
+        }
+      }
     }
   }
   return false;
 }
 
-function retrieve_catagories(mydata){//recursive function to run through JSON data if needed
-  for (var key in mydata) {
-    if(typeof mydata[key] == 'object'){
-      retrieve_catagories(mydata[key]);
-    }else if(!isNaN(mydata[key])){
-      console.log("Key: " + key);
-      console.log("Value: " + mydata[key]);
+
+function array_value_insertion_handler(array, subCategory, groupType, position, difference, obj, sortSelecter) {
+  //This checks if there is a repeat catagory group and handles all cases of value insertion into the array
+  for (let i = 0; i < array.length; i++) {
+    if("subCategory" in array[i][1]){
+      if (subCategory.toString() === array[i][1].subCategory.toString() && array[i][1].groupType.toString() === groupType.toString()) {
+        if(position <= i){
+          array[i][0] = difference;
+          array[i][1] = obj;
+          if(sortSelecter){
+            array.sort(sortFunctionHigher);
+          }else{
+            array.sort(sortFunctionLower);
+          }
+          return array;
+        }else{
+          return array;
+        }
+      }
     }
+  }
+  //this only runs if the inserting value is a unique group or subcategory
+  array[array.length - 1][0] = difference;
+  array[array.length - 1][1] = obj;
+  if(sortSelecter){
+    array.sort(sortFunctionHigher);
+  }else{
+    array.sort(sortFunctionLower);
+  }
+  return array;
+}
+//sorts by high to low
+function sortFunctionHigher(a, b) {
+    if (a[0] === b[0]) {
+        return 0;
+    }
+    else {
+        return (a[0] > b[0]) ? -1 : 1;
+    }
+}
+//sorts by low to high
+function sortFunctionLower(a, b) {
+  if (a[0] === b[0]) {
+      return 0;
+  }
+  else {
+      return (a[0] < b[0]) ? -1 : 1;
   }
 }
